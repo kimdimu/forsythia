a3ef6de74ab4fef51ad8468487c2460073b1f7e7
seongji0125 <49138648+seongji0125@users.noreply.github.com>
2019-09-11 00:17:55 +0900

    GravityFalse

---
+++ Assets/Script/PlanetGravityBody.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGravityBody : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public PlanetGravityAttractor attractor;
+    private Transform myTransform;
+
+    void Start()
+    {
+        //GetComponent<Rigidbody>();
+        //rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        //rigidbody.useGravity = false;
+
+        //myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //attractor.Attract(myTransform);
+    }
+}
