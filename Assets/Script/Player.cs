c08a3acfff05544239275c57fc41d50980777e49
AReum <rkd7007@naver.com>
2019-09-19 20:44:09 +0900

    InGame

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,10 +7,14 @@ public class Player : MonoBehaviour
     public float moveSpeed = 15;
     private Vector3 moveDir;
 
+    //캐릭터가 움직일 스피드
+    public float speed = 10.0f;
+
     void Update()
     {
         moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-    }
+
+   }
 
     void FixedUpdate()
     {
