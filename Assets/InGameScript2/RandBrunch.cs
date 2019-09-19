a6232986e97e7e4bf5bfa84b991941380248e68b
gkgk2412 <54790406+gkgk2412@users.noreply.github.com>
2019-09-19 23:32:55 +0900

    InGame

--- Assets/InGameScript2/RandBrunch.cs
+++ Assets/InGameScript2/RandBrunch.cs
@@ -29,21 +29,21 @@ public class RandBrunch : MonoBehaviour
         tempGO[2] = object3;
         tempGO[3] = object4;
 
-        object1.transform.localScale = new Vector3(276.74f, 687.7819f, 363.8301f);
-        object2.transform.localScale = new Vector3(425.128f, 452.4986f, 363.8301f);
-        object3.transform.localScale = new Vector3(2, 2, 2);
-        object4.transform.localScale = new Vector3(2, 2, 2);
+        object1.transform.localScale = new Vector3(1044.595f, 539.7225f, 539.7225f);
+        object2.transform.localScale = new Vector3(809.9556f, 367.082f, 491.3162f);
+        object3.transform.localScale = new Vector3(1.904798f, 74.37221f, 4.96139f);
+        object4.transform.localScale = new Vector3(1206.267f, 367.082f, 491.3162f);
 
 
-        while (positionCon <= 450)
+        while (positionCon <= 350)
         {
-            Instantiate(tempGO[Random.Range(0, 4)], new Vector3(759.55f, 1164f + positionCon/*이전에 생긴 발판의 y좌표값보다 -0.0364 만큼*/, 0f), Quaternion.Euler(0f, 90f, -90f));
+            Instantiate(tempGO[Random.Range(0, 4)], new Vector3(759.55f, 1190f + positionCon/*이전에 생긴 발판의 y좌표값보다 -0.0364 만큼*/, 0f), Quaternion.Euler(0f, 90f, 0f));
             //object1 Quaternion -> /*Quaternion.Euler(-15.899f, 87.616f, -91.47301f)*/
-            Instantiate(tempGO[Random.Range(0, 4)], new Vector3(756.5f, 1178f + positionCon/*이전에 생긴 발판의 y좌표값보다 -0.0364 만큼*/, 0f), Quaternion.Euler(0f, -90f, 0f));
+            Instantiate(tempGO[Random.Range(0, 4)], new Vector3(756.5f, 1214.8f + positionCon/*이전에 생긴 발판의 y좌표값보다 -0.0364 만큼*/, 0f), Quaternion.Euler(0f, -90f, 0f));
 
             yield return new WaitForSeconds(0.1f);
 
-            positionCon += 30;
+            positionCon += 50;
 
             Debug.Log(positionCon);
 
