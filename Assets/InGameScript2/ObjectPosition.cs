079d5bcf5a0e39db07201e9a6b27fa809b019b65
gkgk2412 <54790406+gkgk2412@users.noreply.github.com>
2019-09-26 18:54:48 +0900

    InGame

--- Assets/InGameScript2/ObjectPosition.cs
+++ Assets/InGameScript2/ObjectPosition.cs
@@ -16,34 +16,40 @@ public class ObjectPosition : MonoBehaviour
 
     List<GameObject> branchList = new List<GameObject> (); //가지객체를 생성하고 관리해줄 리스트 생성
 
+    //GameObject TEST = GameObject.FindGameObjectWithTag("test");
 
 
     public GameObject emptybranch1;
+
+    private Vector3 lastBranchPos;
 
     // Start is called before the first frame update
 
     void Start()
     {
         StartCoroutine(BranchRandomGenerator());
+        lastBranchPos = this.emptybranch1.transform.position;
     }
 
 
     IEnumerator BranchRandomGenerator()
     {
 
-        Vector3 pos1, pos2;
+        Vector3 pos1, pos2, tesT;
         bool isTest = true;
 
         while (true)
         {
 
-            RandQNum = Random.Range(50, 120);//220 ~ 320까지 랜덤으로 숫자 생성
+            RandQNum = Random.Range(220, 320);//220 ~ 320까지 랜덤으로 숫자 생성
 
 
             //emptybranch1.transform.position = Branch1.transform.position; // 초기기둥의 위치를 빈오브젝트의 위치로 넣음
 
-            pos1 = this.emptybranch1.transform.position; //빈오브젝트의 위치 = pos1
-            
+            // pos1 = this.emptybranch1.transform.position; //빈오브젝트의 위치 = pos1
+
+            pos1 = lastBranchPos;
+            //tesT = this.TEST.transform.position;
 
             for (int i = 0; i < 1; i++)
             {
@@ -54,12 +60,16 @@ public class ObjectPosition : MonoBehaviour
 
                     pos2 = branchList[i].transform.position;
 
+                   
 
-
-                    Instantiate(Branch1, new Vector3(pos1.x + pos2.x, pos1.y + pos2.y, pos1.z), Quaternion.Euler(RandQNum, 90f, -90f));
-                }
+}
 
             }
+
+            lastBranchPos = branchList[branchList.Count - 1].transform.GetChild(0).transform.position;
+            //GameObject lastBranch = branchList[branchList.Count - 1];
+            //lastBranch.transform.GetChild(0);
+
 
             //2초후에
             yield return new WaitForSeconds(2f);
