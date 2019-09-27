af1804de6faf6fba3b736b436d479430e42a3baa
gkgk2412 <54790406+gkgk2412@users.noreply.github.com>
2019-09-27 20:24:09 +0900

    InGAMe

--- Assets/InGameScript2/ObjectPosition.cs
+++ Assets/InGameScript2/ObjectPosition.cs
@@ -8,6 +8,8 @@ using UnityEngine;
 
 public class ObjectPosition : MonoBehaviour
 {
+    public float FirstTime; //전체시간으로 사용할 변수
+    public static float LimitTime; //변동시간으로 사용할 변수
 
     //가지 오브젝트
     public GameObject BranchObject1; //일반가지
@@ -52,8 +54,10 @@ public class ObjectPosition : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //코루틴함수 호출
-        StartCoroutine(BranchRandomGenerator());
+        ////코루틴함수 호출
+        //StartCoroutine(BranchRandomGenerator());
+
+
 
         //벡터에 기둥의 빈오브젝트의 위치값을 넣어준다
         lastBranchPos = this.emptybranch1.transform.position;
@@ -66,14 +70,19 @@ public class ObjectPosition : MonoBehaviour
         //벡터에 가지의 왼쪽 빈오브젝트의 위치값을 넣어준다
         LeftBranchPos = this.RandomBranch_Right.transform.position;
 
+        LimitTime = 60;
 
 
     }
 
+    void Update()
+    {
 
+        BranchRandomGenerator();
+    }
 
     //========================기둥, 가지 생성부=========================//
-    IEnumerator BranchRandomGenerator()
+    void BranchRandomGenerator()
     {
         //가지를 랜덤으로 생성할 배열
         GameObject[] BranchArray = new GameObject[4];
@@ -99,9 +108,10 @@ public class ObjectPosition : MonoBehaviour
         //pos1과 pos2 ,pos3를 벡터로 선언
         Vector3 pos1, pos2 ,pos3;
 
-        while (true) //나중에 조건식같은거를 넣어서 줄기생기는 갯수를 통제하도록 하자
+        while (LimitTime >= 0) //나중에 조건식같은거를 넣어서 줄기생기는 갯수를 통제하도록 하자
         {
-           
+            LimitTime -= Time.deltaTime; // 일정한 시간에 따라 감소되도록 만듦
+
             //줄기의 쿼터니온을 조절 (270이 수직으로서는 각도 왼쪽으로 25도, 오른쪽으로 25도 하여 총 50도로 조절)
             RandQNum = Random.Range(245, 295); //245 ~ 295 랜덤으로 숫자 생성
 
@@ -263,9 +273,9 @@ public class ObjectPosition : MonoBehaviour
             //GameObject lastBranch = branchList[branchList.Count - 1];
             //lastBranch.transform.GetChild(0);
 
-
-            //0.5f초후에 다시 while문 돈다.
-            yield return new WaitForSeconds(0.5f);
+            //IEnumerator
+            ////0.5f초후에 다시 while문 돈다.
+            //yield return new WaitForSeconds(0.5f);
 
 
 
