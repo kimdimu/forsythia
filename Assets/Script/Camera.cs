7deead7f0deaa464cdf2c3bb270967fb0388bca8
seongji0125 <49138648+seongji0125@users.noreply.github.com>
2019-09-28 15:12:08 +0900

    Update Camera.cs

--- Assets/Script/Camera.cs
+++ Assets/Script/Camera.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+<<<<<<< HEAD
     public Transform target;    //추적할 타겟
     public float dist = 90f;    //카메라와의 거리
     public float height = 70f;   //카메라의 높이
@@ -16,6 +17,21 @@ public class Camera : MonoBehaviour
         tr = GetComponent<Transform>();
     }
 
+=======
+    public float MoveSpeed;     // 플레이어를 따라오는 카메라 맨의 스피드.
+
+    // 비공개
+    private Transform Target;   // 플레이어의 트랜스 폼.
+    private Vector3 Pos;        // 자신의 위치.
+
+    void Start()
+    {
+        // Player라는 태그를 가진 오브젝트의 transform을 가져온다.
+        Target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // 플레이어를 따라다님.
+>>>>>>> parent of 702ab88... Camera2
     void Update()
     {
         //카메라 y 축을 타겟의 y축 회전각도로 부드럽게 회전
