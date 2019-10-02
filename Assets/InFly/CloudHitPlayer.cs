b2f04dd33ae7618dd452223c77ed91b33c6c48e1
AReum <rkd7007@naver.com>
2019-10-02 16:10:23 +0900

    InGame

---
+++ Assets/InFly/CloudHitPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudHitPlayer : MonoBehaviour
+{
+    float MoreSpeedTime;
+    bool IsHit;
+
+    void Update()
+    {
+        if(IsHit == true)
+        {
+            MoreSpeedTime += Time.deltaTime; //일정한 시간에 따라 증가함
+        }
+        //구름링을 통과한 뒤 1.5초가 지나면
+        if (MoreSpeedTime >= 1.5f)
+        {
+            Fly.BirdSpeed = 100; //속도 낮추기
+            InFlyCamera.speed = 200;
+            MoreSpeedTime = 0f; //시간 초기화
+            IsHit = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") //구름링이 Player 태그와 충돌하면
+        {             
+            Fly.BirdSpeed = 150; //속도 증가
+            InFlyCamera.speed = 300;
+            IsHit = true;
+        }
+    }
+}
--- Assets/InFly/Sun.cs
+++ Assets/InFly/Sun.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Sun : MonoBehaviour
 {
-    public GameObject[] Sphere; //복제 될 햇살
-    GameObject firstground; //처음 햇살 생성 위치
+    public GameObject Sphere; //복제 될 햇살
+    public GameObject[] firstground; //처음 햇살 생성 위치
 
     GameObject obj; //클론 받아오는 오브젝트
     List<GameObject> SunList = new List<GameObject>(); //오브젝트 리스트 생성
@@ -16,7 +16,7 @@ public class Sun : MonoBehaviour
 
     void Start()
     {
-        firstground = GameObject.FindGameObjectWithTag("Sun"); //처음 발판의 위치 받아오기 위해 사용
+        // firstground = GameObject.FindGameObjectWithTag("Sun"); //처음 발판의 위치 받아오기 위해 사용
         SunInIt(); //햇살 생성 시작
     }
 
@@ -32,32 +32,41 @@ public class Sun : MonoBehaviour
     //햇살 생성 시키는 함수
     void SunInIt()
     {
-        //비행 게이지가 끝날 때까지 생성
-        for (int i = 0; i < Fly.FlyTime; i++)
+        //비행 게이지가 끝날 때까지 생성. Fly.FlyTime
+        //세 개만 생성
+        for (int i = 0; i < 3; i++)
         {
-            ranSph = Random.Range(0, 2);
-            ranX = Random.Range(460, 600);
+            //ranSph = Random.Range(0, 2);
+            //ranX = Random.Range(460, 600);
+            ranX = Random.Range(0, 9);
 
-            obj = (GameObject)Instantiate(Sphere[ranSph], firstground.transform.position, firstground.transform.rotation);
+            //obj = (GameObject)Instantiate(Sphere[ranSph], firstground.transform.position, firstground.transform.rotation);
+            obj = (GameObject)Instantiate(Sphere, firstground[ranX].transform.position, firstground[ranX].transform.rotation);
+            obj.transform.localScale = new Vector3(35f, 35f, 5f); //구름링 클론 크기 변경
 
-            if (ranSph == 0)
-                obj.transform.localScale = new Vector3(13f, 13f, 13f); //햇살 클론 크기 변경
-            else
-            {
-                CloudSun++; //구름링 개수 증가
-                obj.transform.localScale = new Vector3(35f, 35f, 5f); //구름링 클론 크기 변경
-            }
             SunList.Add(obj);
 
-            if (ranSph == 1) 
-            {
-                SunList[i].transform.position = new Vector3(ranX, firstground.transform.position.y, transform.position.z + 100 * i);
-            }
-            else
-                SunList[i].transform.position = new Vector3(ranX, firstground.transform.position.y, transform.position.z + 200 * i);
+            SunList[i].transform.position = new Vector3(firstground[ranX].transform.position.x,
+                firstground[ranX].transform.position.y, firstground[ranX].transform.position.z);
 
-            if (CloudSun >= 3) //구름링 3 (또는 4개) 생성 했으면
-                ranSph = 0; //햇살만 생성시킨다 
+            //if (ranSph == 0)
+            //    obj.transform.localScale = new Vector3(13f, 13f, 13f); //햇살 클론 크기 변경
+            //else
+            //{
+            //    CloudSun++; //구름링 개수 증가
+            //    obj.transform.localScale = new Vector3(35f, 35f, 5f); //구름링 클론 크기 변경
+            //}
+            //SunList.Add(obj);
+
+            //if (ranSph == 1) 
+            //{
+            //    SunList[i].transform.position = new Vector3(ranX, firstground.transform.position.y, transform.position.z + 100 * i);
+            //}
+            //else
+            //    SunList[i].transform.position = new Vector3(ranX, firstground.transform.position.y, transform.position.z + 200 * i);
+
+            //if (CloudSun >= 3) //구름링 3 (또는 4개) 생성 했으면
+            //    ranSph = 0; //햇살만 생성시킨다 
 
         }
     }
