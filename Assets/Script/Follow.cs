7331c508265e2667959a87bb335149125e536bb0
seongji0125 <49138648+seongji0125@users.noreply.github.com>
2019-10-16 05:07:58 +0900

    Guard

--- Assets/Script/Follow.cs
+++ Assets/Script/Follow.cs
@@ -8,6 +8,8 @@ public class Follow : MonoBehaviour
     public Quaternion targetRote; 
     public float fSpeed = 2.0f;
 
+    private float fHeight = 0.0f;
+
     private Vector3 v;
     // Start is called before the first frame update
     void Start()
@@ -19,9 +21,10 @@ public class Follow : MonoBehaviour
     void Update()
     {
         transform.Rotate(270 ,45,-270); //이새끼 왜 로테이션이 쳐안먹히는거야
-        
-        
-        Vector3 Pos = target.position + new Vector3(0.0f, 20.0f, 0.0f); //거리
+
+        fHeight = Random.Range(19.0f, 25.0f); //펄럭펄럭 
+
+        Vector3 Pos = target.position + new Vector3(0.0f, fHeight, 0.0f); //거리
 
         transform.position = Vector3.Lerp(transform.position, Pos, Time.deltaTime * fSpeed);//거리가 생기면 따라오도록
         //transform.position += (target.position - Pos) * fSpeed;
