5b42bb2c6fc8dbfb4f4149e1f2500c4806b0bdb1
seongji0125 <49138648+seongji0125@users.noreply.github.com>
2019-10-16 05:01:11 +0900

    Update Follow.cs

--- Assets/Script/Follow.cs
+++ Assets/Script/Follow.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class Follow : MonoBehaviour
 {
     public Transform target;
-
-    private float Height = 10.0f;
-    private float Distance = -1.0f;
+    public Quaternion targetRote; 
     public float fSpeed = 2.0f;
 
+    private Vector3 v;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +18,14 @@ public class Follow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 0);
-        Vector3 Pos = target.position + new Vector3(5.0f, Height, 5.0f); //거리
+        transform.Rotate(0,20,0);
+        
+        
+        Vector3 Pos = target.position + new Vector3(0.0f, 20.0f, 0.0f); //거리
 
-        transform.position = Vector3.Lerp(transform.position, Pos, Time.deltaTime * fSpeed); //거리가 생기면 따라오도록
+        transform.position = Vector3.Lerp(transform.position, Pos, Time.deltaTime * fSpeed);//거리가 생기면 따라오도록
         //transform.position += (target.position - Pos) * fSpeed;
+        //transform.position -= new Vector3(3.0f, 0, 3.0f);
         transform.LookAt(target);
     }
 }
