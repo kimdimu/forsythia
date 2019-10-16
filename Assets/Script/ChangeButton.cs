f888cba9310930b5fbf9edc053c617693dd825a5
AReum <rkd7007@naver.com>
2019-10-16 19:03:34 +0900

    InGame

---
+++ Assets/Script/ChangeButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeButton : MonoBehaviour
+{
+    public GameObject St;
+    public GameObject We;
+
+    Vector3 StVector; //st위치
+    Vector3 WeVector; //we위치
+    Vector3 TempVector; //위치 저장할 변수
+
+    public static bool IsChange; //바뀌었는지 안 바뀌었는지
+
+    void Start()
+    {
+       // IsChange = false; //안 바뀐 상태로 시작 (강 / 약)
+        StVector = St.transform.position; //st의 처음위치 저장
+        WeVector = We.transform.position; //we의 처음위치 저장
+    }
+
+    void Update()
+    {
+        St.transform.position = StVector;
+        We.transform.position = WeVector;
+    }
+
+    public void ChangeBtton()
+    {
+        if (IsChange) //바뀌었으면
+        {
+            TempVector = StVector; //st위치를 temp에 저장
+            StVector = WeVector; //we위치를 st에 넣음 
+            WeVector = TempVector; //temp위치를 we에 저장
+            IsChange = false;
+        }
+        else if (!IsChange) //안 바뀌었으면
+        {
+            TempVector = StVector;
+            StVector = WeVector;
+            WeVector = TempVector;
+            IsChange = true;
+        }
+    }
+}
---
+++ Assets/Script/IsCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsCamera : MonoBehaviour
+{
+    public GameObject OptionBtn;
+    public GameObject CameraBtn;
+    public GameObject MissionBtn;
+    public GameObject CameraPanmel;
+
+    Vector3 _CameraBut; //카메라 버튼의 원래 위치
+    Vector3 newCameraBut; //카메라 버튼의 새로운 위치
+
+    void Start()
+    {
+        _CameraBut = CameraBtn.transform.position; //원래 위치 저장
+        newCameraBut = new Vector3(-100, 100, 0); //새로운 위치 지정
+        CameraPanmel.SetActive(false);     
+    }
+
+    void Update()
+    {
+        if (ButtonMa.IsCameraClick) //카메라 눌렀으면
+        {
+            OptionBtn.SetActive(false); //설정 버튼 안 보이게 함
+            MissionBtn.SetActive(false); //미션 버튼 안 보이게 함
+            CameraBtn.transform.position = newCameraBut; //새로운 위치로 이동시킴
+            CameraPanmel.SetActive(true); //카메라 팝업 보이게 함
+        }
+
+        if (!ButtonMa.IsCameraClick) //카메라 안 눌렀으면
+        {
+            OptionBtn.SetActive(true); //설정 버튼 보이게함
+            MissionBtn.SetActive(true); //미션 버튼 보이게 함
+            CameraBtn.transform.position = _CameraBut; //원래 위치로 돌아옴
+            CameraPanmel.SetActive(false); //카메라 팝업 안 보이게 함
+        }
+    }
+}
