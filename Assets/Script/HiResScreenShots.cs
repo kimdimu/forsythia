35a277d73946185bc7eb039dd3b9256a19d941f9
AReum <rkd7007@naver.com>
2019-10-16 23:38:47 +0900

    InGame

--- Assets/Script/HiResScreenShots.cs
+++ Assets/Script/HiResScreenShots.cs
@@ -1,56 +1,99 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
+//using System.Collections;
+
+//public class HiResScreenShots : MonoBehaviour
+//{
+//    private int resWidth = 1280;
+//    private int resHeight = 752;
+
+//    private bool takeHiResShot = false;
+
+//    void Awake()
+//    {
+//        resWidth = Screen.width;
+//        resHeight = Screen.height;
+//    }
+
+//    public static string ScreenShotName(int width, int height)
+//    {
+//        return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png",
+//                             Application.dataPath,
+//                             width, height,
+//                             System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+//    }
+
+//    public void TakeHiResShot()
+//    {
+//        takeHiResShot = true;
+//    }
+
+//    void LateUpdate()
+//    {
+//        takeHiResShot |= Input.GetKeyDown("k");
+//        if (takeHiResShot)
+//        {
+//            takeHiResShot = false;
+
+//            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+//            //camera.targetTexture = rt;
+//            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+//            //camera.Render();
+//            RenderTexture.active = rt;
+//            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+//            //camera.targetTexture = null;
+//            RenderTexture.active = null; // JC: added to avoid errors
+//            Destroy(rt);
+
+//            // 파일로 저장.
+//            byte[] bytes = screenShot.EncodeToPNG();
+//            string filename = ScreenShotName(resWidth, resHeight);
+//           // File.WriteAllBytes("mnt/sdcard/DCIM/Camera/number_" + Time.time.ToString() + ".png", bytes);
+//            //System.IO.File.WriteAllBytes(filename, bytes);
+//            Debug.Log(string.Format("Took screenshot to: {0}", filename));
+
+//        }
+//    }
+//}
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
 
 public class HiResScreenShots : MonoBehaviour
 {
-    private int resWidth = 1280;
-    private int resHeight = 752;
+    public Camera _camera;       //보여지는 카메라.
 
-    private bool takeHiResShot = false;
+    private int resWidth;
+    private int resHeight;
+    string path;
 
-    void Awake()
+    void Start()
     {
         resWidth = Screen.width;
         resHeight = Screen.height;
+        path = Application.dataPath + "/ScreenShot/";
+        Debug.Log(path);
     }
 
-    public static string ScreenShotName(int width, int height)
+    public void ClickScreenShot()
     {
-        return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png",
-                             Application.dataPath,
-                             width, height,
-                             System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
-    }
-
-    public void TakeHiResShot()
-    {
-        takeHiResShot = true;
-    }
-
-    void LateUpdate()
-    {
-        takeHiResShot |= Input.GetKeyDown("k");
-        if (takeHiResShot)
+        DirectoryInfo dir = new DirectoryInfo(path);
+        if (!dir.Exists)
         {
-            takeHiResShot = false;
-
-            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
-            //camera.targetTexture = rt;
-            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
-            //camera.Render();
-            RenderTexture.active = rt;
-            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
-            //camera.targetTexture = null;
-            RenderTexture.active = null; // JC: added to avoid errors
-            Destroy(rt);
-
-            // 파일로 저장.
-            byte[] bytes = screenShot.EncodeToPNG();
-            string filename = ScreenShotName(resWidth, resHeight);
-           // File.WriteAllBytes("mnt/sdcard/DCIM/Camera/number_" + Time.time.ToString() + ".png", bytes);
-           // System.IO.File.WriteAllBytes(filename, bytes);
-            Debug.Log(string.Format("Took screenshot to: {0}", filename));
-
+            Directory.CreateDirectory(path);
         }
+        string name;
+        name = path + System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
+        RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+       // _camera.targetTexture = rt;
+        Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+        Rect rec = new Rect(0, 0, screenShot.width, screenShot.height);
+        //_camera.Render();
+        RenderTexture.active = rt;
+        screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+        screenShot.Apply();
+
+        byte[] bytes = screenShot.EncodeToPNG();
+        File.WriteAllBytes(name, bytes);
     }
 }
--- Assets/Script/NewBehaviourScript.cs
+++ Assets/Script/NewBehaviourScript.cs
@@ -2,27 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonMa : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-    public static bool OptionClick = false;
-    public static bool IsCameraClick = false;
-
-    public void IsOption()
-    {
-        if (OptionClick)
-            OptionClick = false;
-        else if (!OptionClick)
-            OptionClick = true;
-    }
-
-    public void CameraClick()
-    {
-        if (IsCameraClick)
-            IsCameraClick = false;
-        else if (!IsCameraClick)
-            IsCameraClick = true;
-    }
-
     public void IsMission()
     {
         StartCoroutine("Rendering");
@@ -35,7 +16,7 @@ public class ButtonMa : MonoBehaviour
         byte[] imgBytes;
         //string path = @"G:\SreenShot\test.png";
         string path = @"C:\Users\403\Documents\GitHub\forsythia\ScreenShot\test.png";
-        Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+         Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
         texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0, false);
         texture.Apply();
 
