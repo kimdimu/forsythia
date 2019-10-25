c156107cd4ad75d276003579547e94432c3f6faa
AReum <rkd7007@naver.com>
2019-10-25 18:36:45 +0900

    InMain

--- Assets/Script/Option.cs
+++ Assets/Script/Option.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Option : MonoBehaviour
 {
+    public GameObject optoinPanel;
+
     void Start()
     {
-        
+        optoinPanel.SetActive(false);
     }
     
     void Update()
